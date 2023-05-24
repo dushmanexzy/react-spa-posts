e1d37f80aea8622deeec7b204b9516b88d795f1a
Andrey Dvortsov <exzy92@gmail.com>
2023-05-24 19:52:28 +0300

    Spinner added

--- src/pages/home/components/CommentList/CommentList.js
+++ src/pages/home/components/CommentList/CommentList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Spinner from "react-bootstrap/Spinner";
 import {useCommentData} from "../../../../hooks/useCommentData";
 import {CommentListItem} from "../CommentListItem/CommentListItem";
 
@@ -6,13 +7,25 @@ import "./commentlist.css";
 
 export function CommentList({postId}) {
   const comments = useCommentData(postId);
+  const [prepared, setPrepared] = React.useState(false);
+
+  React.useEffect(() => {
+    if (comments[0].length) setPrepared(true);
+  }, [comments])
+
 
   return (
     <div className={`comments-list`}>
-      <h3 className={`comments-list-title`}>Комментарии:</h3>
-      {comments[0].map(comment => (
-        <CommentListItem key={comment.id} comment={comment} />
-      ))}
+      {prepared ?
+        <>
+          <h3 className={`comments-list-title`}>Комментарии:</h3>
+          {comments[0].map(comment => (
+            <CommentListItem key={comment.id} comment={comment}/>
+          ))}
+        </> :
+        <Spinner animation={'border'}>
+          <span className={'visually-hidden'}>Loading...</span>
+        </Spinner>
+      }
     </div>
-  );
-}
+)}
