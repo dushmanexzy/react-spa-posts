6df79161bb2e5a415ac2514ca735a6a129281948
Andrey Dvortsov <exzy92@gmail.com>
2023-05-24 18:53:39 +0300

    add comments to post

--- src/pages/home/components/PostListItem/PostListItem.js
+++ src/pages/home/components/PostListItem/PostListItem.js
@@ -4,27 +4,38 @@ import {Link} from "react-router-dom";
 import {capitalizeText} from "../../../../utils/capitalizeText";
 
 import './postlistitem.css';
+import {CommentList} from "../CommentList/CommentList";
 
 export function PostListItem({post}) {
+  const [isCommentsOpened, setIsCommentsOpened] = React.useState(false);
   const {id, userId, title, body} = post;
 
-  return (
-      <Card className={`post-container`}>
-        <Card.Body className={`post`}>
-          <Link to={'/more'}>
-            <Image
-              className={`avatar`}
-              src={`https://cdn-icons-png.flaticon.com/512/3607/3607444.png`}
-            />
-          </Link>
-          <Card.Title className={`post-title`}>{capitalizeText(title)}</Card.Title>
-          <Card.Text className={`post-text`}>{capitalizeText(body)}</Card.Text>
+  const toggleCommentsClick = React.useCallback(() => {
+    setIsCommentsOpened(prevState => !prevState)
+  }, [isCommentsOpened])
 
-          <div className={`comments`}>
-            Комментарии
-          </div>
-        </Card.Body>
-        <Button className={`primary post-text`}>Комментарии</Button>
-      </Card>
+  return (
+    <Card className={`post-container`}>
+      <Card.Body className={`post`}>
+        <Link to={'/more'}>
+          <Image
+            className={`avatar`}
+            src={`https://cdn-icons-png.flaticon.com/512/3607/3607444.png`}
+          />
+        </Link>
+        <Card.Title className={`post-title`}>{capitalizeText(title)}</Card.Title>
+        <Card.Text className={`post-text`}>{capitalizeText(body)}</Card.Text>
+
+        {isCommentsOpened && (
+          <CommentList postId={id}/>
+        )}
+      </Card.Body>
+      <Button
+        className={`primary post-text`}
+        onClick={toggleCommentsClick}
+      >
+        {isCommentsOpened ? 'Скрыть комментарии' : 'Комментарии'}
+      </Button>
+    </Card>
   );
 }
