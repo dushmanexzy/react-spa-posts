5ebe49291744ada038b0b2608efd9c7b7c56fc4b
Andrey Dvortsov <exzy92@gmail.com>
2023-05-24 18:53:21 +0300

    remove context for comments

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import {PostsList} from "./components";
 import {PostContextProvider} from "../../context/PostContext";
-import {CommentContextProvider} from "../../context/CommentContext";
 
 export function Home() {
 
   return (
     <PostContextProvider>
-      <CommentContextProvider>
-        <h1 className={`main-title`}>Список комментариев</h1>
+      <h1 className={`main-title`}>Список комментариев</h1>
 
-        <PostsList/>
-      </CommentContextProvider>
+      <PostsList/>
     </PostContextProvider>
   );
 }
