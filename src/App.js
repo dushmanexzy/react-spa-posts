c6fccc8e26ccdf3f9a10aeaa1869c32b810257fd
Andrey Dvortsov <exzy92@gmail.com>
2023-05-25 17:32:25 +0300

    update App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Home} from "./pages/home/Home";
+import {Header} from "./Header/Header";
 
 import './App.css';
 
@@ -17,13 +18,19 @@ function More() {
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={'/more'} element={<More />}/>
-        <Route path={'/about'} element={<About />}/>
-        <Route path={'/'} element={<Home />}/>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Header />
+
+      <main className="main">
+        <BrowserRouter>
+          <Routes>
+            <Route path={'/more'} element={<More/>}/>
+            <Route path={'/about'} element={<About/>}/>
+            <Route path={'/'} element={<Home/>}/>
+          </Routes>
+        </BrowserRouter>
+      </main>
+    </>
   );
 }
 
